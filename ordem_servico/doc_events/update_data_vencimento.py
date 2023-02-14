b054f11e734240703cadbf1cf0afb7ab123bcc5a
Lucas Matheus <lucasmatheusc@hotmail.com>
2023-02-14 17:07:03 +0000

    alteração nas permissões para adicionar ToDo

--- ordem_servico/doc_events/update_data_vencimento.py
+++ ordem_servico/doc_events/update_data_vencimento.py
@@ -2,10 +2,11 @@ from xml.dom.minidom import Document
 from frappe import _, throw
 import frappe
 
-
 def update_data_vencimento(doc, method):
+
+   
     opportunity = frappe.get_doc("Opportunity", doc.reference_name)
-    data_vencimento = frappe.get_list("Data de Vencimento Taks", filters={"parent": opportunity.name, "reference_task": doc.name})
+    data_vencimento = frappe.get_all("Data de Vencimento Taks", filters={"parent": opportunity.name, "reference_task": doc.name})
 
     if doc.status == "Closed":
         if data_vencimento:
@@ -17,12 +18,10 @@ def update_data_vencimento(doc, method):
             if data_vencimento[0].data_vencimento != doc.date:
                 data_vencimento = frappe.get_doc("Data de Vencimento Taks", data_vencimento[0].name)
                 data_vencimento.data_vencimento = doc.date
-                data_vencimento.save()
+                data_vencimento.save(ignore_permissions=True)
                 frappe.publish_realtime("doc_update", {"doctype": "Opportunity", "name": opportunity.name})
 
-                
-                
-            
+                            
         else:
             data_vencimento = frappe.get_doc({
                 "doctype": "Data de Vencimento Taks",
@@ -32,6 +31,7 @@ def update_data_vencimento(doc, method):
                 "data_vencimento": doc.date,
                 "reference_task": doc.name
             })
-            data_vencimento.save()
+          
+            data_vencimento.save(ignore_permissions=True)
             frappe.publish_realtime("doc_update", {"doctype": "Opportunity", "name": opportunity.name})
 
---
+++ ordem_servico/ordem_servico/doctype/data_de_vencimento_taks/data_de_vencimento_taks.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, laugusto and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Data de Vencimento Taks', {
+	// refresh: function(frm) {
+
+	// }
+});
