d93d0946d9de629684e2848b1c55d134b528cf65
Lucas Matheus <lucasmatheusc@hotmail.com>
2023-03-13 13:22:59 +0000

    Ocultar campo de verificação no address, se o endereço é corporativo, modificação na precisão da porcentagem no payment term e payment schedule

--- ordem_servico/hooks.py
+++ ordem_servico/hooks.py
@@ -61,7 +61,9 @@ fixtures = [
              'Purchase Order Item',
              'Customer',
              'Client Script',
-             'Opportunity'
+             'Opportunity',
+             'Payment Schedule',
+             'Payment Term'
              
          )
      ]]
@@ -81,7 +83,9 @@ fixtures = [
              'Purchase Order Item',
              'Customer',
              'Client Script',
-             'Opportunity'
+             'Opportunity',
+             'Payment Schedule',
+             'Payment Term'
              
          )
      ]]
