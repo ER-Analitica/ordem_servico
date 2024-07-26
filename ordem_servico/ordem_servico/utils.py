9439c3040999cf0405758fa827d9ddcdc8b736e3
Lucas Matheus <lucasmatheusc@hotmail.com>
2024-07-26 13:41:09 +0000

    pegar empresa cadastrada com base no cliente e permitir edição da sessão additional info em quotation

--- ordem_servico/ordem_servico/utils.py
+++ ordem_servico/ordem_servico/utils.py
@@ -130,6 +130,9 @@ def make_quotation(os_docname):
     quot_doc.rouding_adjustment = 0
     quot_doc.tc_name = "Boleto 15 dias"
     quot_doc.hash_orc = "1"
+    customer_company = frappe.db.get_value("Customer", quot_doc.party_name, "company")
+    if customer_company:
+        quot_doc.company = customer_company
    
     
     #quot_doc.terms = frappe.db.get_value(
