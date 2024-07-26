ea39fc8e6ca32d1062d89740057e1898ad6ae4a2
Lucas Matheus <lucasmatheusc@hotmail.com>
2024-07-26 14:15:29 +0000

    Puxar empresa cadastrada no cliente para o orçamento ao gerar uma ordem de servico interna

--- ordem_servico/ordem_servico/utils.py
+++ ordem_servico/ordem_servico/utils.py
@@ -130,6 +130,7 @@ def make_quotation(os_docname):
     quot_doc.rouding_adjustment = 0
     quot_doc.tc_name = "Boleto 15 dias"
     quot_doc.hash_orc = "1"
+    #Trazer cadastro da empresa no cliente ao gerar orçamento
     customer_company = frappe.db.get_value("Customer", quot_doc.party_name, "company")
     if customer_company:
         quot_doc.company = customer_company
