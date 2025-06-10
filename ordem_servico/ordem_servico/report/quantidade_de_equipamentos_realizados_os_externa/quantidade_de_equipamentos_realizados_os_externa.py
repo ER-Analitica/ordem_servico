bc82a75b67f76f387198392b11c971915554ed1a
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-06-10 08:33:37 -0300

    Relatórios quantidade de equipamentos na os externa

---
+++ ordem_servico/ordem_servico/report/quantidade_de_equipamentos_realizados_os_externa/quantidade_de_equipamentos_realizados_os_externa.py
@@ -0,0 +1,53 @@
+import frappe
+from datetime import datetime
+
+def execute(filters=None):
+    from_date = None
+    to_date = None
+    if filters:
+        from_date = datetime.strptime(filters.get("from_date"), "%Y-%m-%d") if filters.get("from_date") else None
+        to_date = datetime.strptime(filters.get("to_date"), "%Y-%m-%d") if filters.get("to_date") else None
+
+    columns = [
+        {"label": "Nome", "fieldname": "name", "fieldtype": "Link", "options": "Ordem Servico Externa", "width": 350},
+        {"label": "Cliente", "fieldname": "customer", "fieldtype": "Data", "width": 200},
+        {"label": "Status", "fieldname": "status_order_service", "fieldtype": "Data", "width": 200},
+        {"label": "Finalizado Em", "fieldname": "end_repair_time", "fieldtype": "Date", "width": 200},
+    ]
+
+    docs = frappe.get_all(
+        "Ordem Servico Externa",
+        filters={"status_order_service": "Encerrada"},
+        fields=["name", "customer", "status_order_service", "end_repair_time"]
+    )
+
+    data = []
+    for doc in docs:
+        if not doc.end_repair_time:
+            continue  # Ignora OS sem data
+
+        try:
+            dt = datetime.strptime(doc.end_repair_time, "%d-%m-%Y %H:%M:%S")
+        except Exception:
+            try:
+                dt = datetime.strptime(doc.end_repair_time, "%d-%m-%Y")
+            except:
+                continue  # Ignora se falhar o parse
+
+        if from_date and dt < from_date:
+            continue
+        if to_date and dt > to_date:
+            continue
+
+        doc.end_repair_time = dt.strftime("%Y-%m-%d")
+        data.append(doc)
+
+    # Adiciona linha total
+    data.append({
+        "name": f"Total: {len(data)} equipamentos realizados nesse período",
+        "customer": "",
+        "status_order_service": "",
+        "end_repair_time": ""
+    })
+
+    return columns, data
