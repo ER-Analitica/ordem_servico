b766ce02cc1309bf8481d292b566a35611fd9a67
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-08-05 12:20:40 +0000

    alterando de forma automática status da os interna para em conserto em casos de orcamento aprovado

--- ordem_servico/doc_events/pre_orcamento_status_em_conserto.py
+++ ordem_servico/doc_events/pre_orcamento_status_em_conserto.py
@@ -3,9 +3,15 @@ from pydoc import doc
 import frappe
 from frappe.utils import flt
 
-def validate(self, method):
-    if self.have_quotation == 1 and self.has_quotation_link != "":
+def before_save(self, method):
+    if (
+        self.have_quotation == 1
+        and self.has_quotation_link
+        and self.has_quotation_link.strip() != ""
+        and self.status_order_service == "Em Recebimento"
+    ):
         self.status_order_service = "Em Conserto"
+
 
 
 
