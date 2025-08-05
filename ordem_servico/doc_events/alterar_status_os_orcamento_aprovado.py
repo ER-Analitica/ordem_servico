edd0cb06467e495bde25ba0bb08a70912a5b2447
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-08-05 15:35:31 -0300

    correção alterando status os interna para em conserto, ao aprovar orcamento

--- ordem_servico/doc_events/alterar_status_os_orcamento_aprovado.py
+++ ordem_servico/doc_events/alterar_status_os_orcamento_aprovado.py
@@ -6,7 +6,7 @@ from frappe.utils import flt
 def on_update_after_submit(self, method):
     previous = self.get_doc_before_save()
 
-    if previous and previous.workflow_state == "Pendente de Aprovação" and self.workflow_state == "Aprovado":
+    if previous and previous.workflow_state == "Pendente de Aprovação" and self.workflow_state == "Aprovado" and self.os_interna_link and self.os_interna_link.strip() != "":
         os_doc = frappe.get_doc("Ordem Servico Interna", self.os_interna_link)
         os_doc.status_order_service = "Em Conserto"
         os_doc.save(ignore_permissions=True)
