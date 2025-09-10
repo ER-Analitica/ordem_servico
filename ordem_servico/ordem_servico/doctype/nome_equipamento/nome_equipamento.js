1612d5b13ba41f4dc6c8b23b27b1a1ca05b7cd10
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-09-10 13:47:40 -0300

    inserindo campo de contato nos equipamentos

---
+++ ordem_servico/ordem_servico/doctype/nome_equipamento/nome_equipamento.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, laugusto and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Nome Equipamento", {
+// 	refresh(frm) {
+
+// 	},
+// });
