466d7160c8c87808dd6360c2f2766761ae4011ae
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-09-10 14:33:03 +0000

    inserindo campo de contato nos equipamentos

--- ordem_servico/ordem_servico/doctype/equipamentos/equipamentos.js
+++ ordem_servico/ordem_servico/doctype/equipamentos/equipamentos.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Equipamentos', {
-	// refresh: function(frm) {
-
-	// }
+    refresh(frm) {
+        frm.fields_dict.contact_link.get_query = function(doc) {
+            return {
+                query: "frappe.contacts.doctype.contact.contact.contact_query",
+                filters: {
+                    link_doctype: "Customer",
+                    link_name: doc.customer
+                }
+            };
+        };
+    }
 });
