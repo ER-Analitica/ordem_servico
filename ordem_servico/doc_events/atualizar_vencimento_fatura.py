788551668fd3452bbf7a2b37ff5e14907675d52d
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-10-21 18:16:08 +0000

    adicionando data do próximo vencimento no campo criado para envio de notificações automáticas por e-mail

--- ordem_servico/doc_events/atualizar_vencimento_fatura.py
+++ ordem_servico/doc_events/atualizar_vencimento_fatura.py
@@ -9,12 +9,13 @@ def atualizar_vencimento_fatura(doc, method):
     """
     if not doc.payment_schedule:
         doc.db_set("custom_proximo_vencimento", "Fatura quitada")
+        doc.db_set("custom_proximo_vencimento_email_automatico", None)
         doc.db_set("custom_proxima_parcela", "Nenhuma parcela pendente")
         return
 
     # percorre a payment_schedule e pega a primeira parcela
     primeira_parcela = doc.payment_schedule[0]
-
+    due_date = primeira_parcela.due_date
     # Formatar data no padrão brasileiro
     data_formatada = formatdate(primeira_parcela.due_date, "dd/mm/yyyy")
 
@@ -24,4 +25,5 @@ def atualizar_vencimento_fatura(doc, method):
         .replace(",", ".", 1)
 
     doc.db_set("custom_proximo_vencimento", data_formatada)
+    doc.db_set("custom_proximo_vencimento_email_automatico", due_date)
     doc.db_set("custom_proxima_parcela", valor_formatado)
