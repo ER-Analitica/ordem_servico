0c4eff828080ed2a750af3ab42b867837df34eee
Lucas Matheus <lucasmatheusc@hotmail.com>
2025-12-09 01:07:43 +0000

    adicionando campo de pontos de calibração + correção na validação do equipamento

--- ordem_servico/doc_events/validacao_equipamento_ordem_servico.py
+++ ordem_servico/doc_events/validacao_equipamento_ordem_servico.py
@@ -4,7 +4,13 @@ from frappe.utils import flt
 
 def validacao_equipamento_ordem_servico(self, method):
     if self.serie_number:
+        if not self.informe_numero_serie:
+            self.informe_numero_serie = self.serie_number
         equipamento_cliente = frappe.db.get_value("Equipamentos", self.informe_numero_serie, "customer")
 
         if self.customer != equipamento_cliente:
             frappe.throw(("O equipamento com o número de série {0} não pertence ao cliente {1}.").format(self.serie_number, self.customer))
+        else:
+            if not self.informe_numero_serie:
+                self.informe_numero_serie = self.serie_number
+    
