7873afa8ce2a36ddd5b90695d0c1282af95e218e
Lucas <lucas.campos@eranalitica.com.br>
2026-05-27 10:37:27 -0300

    função calcular total de tempo execucao servico inserida

--- ordem_servico/doc_events/calcular_total_tempo_execucao_servico.py
+++ ordem_servico/doc_events/calcular_total_tempo_execucao_servico.py
@@ -5,7 +5,7 @@ from frappe.utils import format_duration
 
 def calcular_total_tempo_execucao(doc, method=None):
     total_segundos = sum(
-        (item.custom_custom_tempo_de_execucao or 0)
+        (item.custom_custom_tempo_de_execucao or 0) * (item.qty or 0)
         for item in (doc.items or [])
     )
     doc.custom_total_tempo_de_execucao_servico = (
